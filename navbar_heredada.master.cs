7dbba7e985ef443ededf430a30159d298968fe41
dai <dai@itam.mx>
2018-10-23 09:57:45 -0500

    se cambiaron los valores de conexion y se hizo refactor de navbar_botones

--- navbar_heredada.master.cs
+++ navbar_heredada.master.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class navbar : System.Web.UI.MasterPage
+public partial class navbar_heredada : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -15,7 +15,7 @@ public partial class navbar : System.Web.UI.MasterPage
     protected void btnAddToCart_Click(object sender, EventArgs e)
     {
         Carrito car = (Carrito)Session["carrito"];
-        foreach(GridViewRow row in this.gridCanciones.Rows)
+        foreach (GridViewRow row in this.gridCanciones.Rows)
         {
             if (((CheckBox)row.FindControl("chkComprar")).Checked)
             {
@@ -28,6 +28,6 @@ public partial class navbar : System.Web.UI.MasterPage
     protected void btnPagar_Click(object sender, EventArgs e)
     {
         this.btnAddToCart_Click(null, null);
-        Server.Transfer("pagComprar.aspx");
+        Server.Transfer("pagConfirmacion.aspx");
     }
 }
--- pagInicio.aspx
+++ pagInicio.aspx
@@ -1,10 +1,8 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/navbar_botones.master" AutoEventWireup="true" CodeFile="pagInicio.aspx.cs" Inherits="pagInicio" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/navbar_heredada.master" AutoEventWireup="true" CodeFile="pagInicio.aspx.cs" Inherits="pagInicio" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
-</asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+
+<asp:Content ID="Content1" runat="server" contentplaceholderid="ContentPlaceHolder1">
     <asp:Label ID="lblBienvenida" runat="server" Text="lbl mensaje bienvenida"></asp:Label>
-    <br />
-    <br />
 </asp:Content>
 
